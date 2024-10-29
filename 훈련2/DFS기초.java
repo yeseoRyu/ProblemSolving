0f36268119c8aeb33588df01c1aec58bd2742612
dolchvita <yesoe0502@naver.com>
2024-10-29 14:45:25 +0900

    1028

---
+++ 훈련2/DFS기초.java
@@ -0,0 +1,33 @@
+import java.util.*;
+public class DFS기초 {
+    static String name = "ABTQVX";
+    static ArrayList<int[]> arr = new ArrayList<>(6);
+
+    
+    static void run(int now){
+        System.out.print(name.charAt(now));
+        
+
+        int[] temp = arr.get(now);
+        for(int i = 0; i < temp.length; i++){
+            int next = temp[i];
+            run(next);
+        }
+    }
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        arr.add(new int[]{1, 2, 3});
+        arr.add(new int[]{4, 5});
+        arr.add(new int[]{});
+        arr.add(new int[]{});
+        arr.add(new int[]{});
+        arr.add(new int[]{});
+
+        run(0);
+
+        sc.close();
+    }//main./
+
+}
---
+++ 훈련2/DFS기초2.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class DFS기초2 {
+    static char[] vect = new char[100];
+
+    static void run(int now){
+        if(vect[now] == 0) return;
+
+        System.out.print(vect[now]);
+
+        run(now*2);
+        run(now*(2+1));
+    }
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        vect[1] = 'A';
+        vect[2] = 'B';
+        vect[3] = 'T';
+        vect[4] = 'R';
+        vect[5] = 'S';
+        vect[6] = 'V';
+
+        run(1);
+        sc.close();
+    }//main./
+
+}
