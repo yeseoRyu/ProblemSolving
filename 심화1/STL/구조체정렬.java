4f5d1fb5eddf5beb38e9d77741ce0c53962a3d80
dolchvita <yesoe0502@naver.com>
2025-01-09 21:47:05 +0900

    0109

---
+++ 심화1/STL/구조체정렬.java
@@ -0,0 +1,50 @@
+package STL;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+class Node{
+    int n;
+    char ch;
+    
+    public Node(int n, char ch){
+        this.n = n;
+        this.ch = ch;
+    }
+}
+
+public class 구조체정렬 {
+    static int sort(Node a, Node b){
+        if(a.n > b.n) return 1;
+        else if(a.n == b.n && a.ch > b.ch) return 1;
+        return 0;
+    }
+
+    public static void main(String[] arg){
+        ArrayList<Node> arr = new ArrayList<>();
+        ArrayList<Node> result = new ArrayList<>();
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i = 0; i < n; i++){
+            int t = sc.nextInt();
+            char c = sc.next().charAt(0);
+            arr.add(new Node(t, c));
+        }
+
+        for(int i = 0; i < n; i++){
+            result.add(arr.get(i));
+            if(i == 0) continue;
+            for(int x = i; x> 0; x--){
+                if(sort(result.get(x-1), result.get(x)) == 1){
+                    Collections.swap(result, x-1, x);
+                }
+            }
+        }
+
+        for(int i = 0; i < n; i++){
+            Node node = result.get(i);
+            System.out.println(node.n + " " + node.ch);
+        }
+    }
+}
---
+++ 심화1/STL/점수삽입정렬.java
@@ -0,0 +1,56 @@
+package STL;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+class Node{
+    String name;
+    int score;
+    
+    public Node(String name, int score){
+        this.name = name;
+        this.score = score;
+    }
+}
+
+public class 점수삽입정렬 {
+    static int sort(Node a, Node b){
+        if(a.score < b.score) return 1;
+        else if(a.score == b.score) return 1;
+        return 0;
+    }
+
+
+    static void printResult(ArrayList<Node> result){
+        for(int i = 0; i < result.size(); i++){
+            System.out.print(result.get(i).name + " ");
+            if(i == 2) break;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] arg){
+        Scanner sc = new Scanner(System.in);
+        ArrayList<Node> array = new ArrayList<>();
+        ArrayList<Node> result = new ArrayList<>();
+        int n = sc.nextInt();
+
+        for(int i = 0; i < n; i++){
+            String t = sc.next();
+            int s = sc.nextInt();
+            array.add(new Node(t, s));
+        }
+
+        for(int i = 0; i < n; i++){
+            result.add(array.get(i));
+
+            for(int x = 0; x < i; x++){
+                if(sort(result.get(x), result.get(i)) == 1){
+                    Collections.swap(result, x, i);
+                }
+            }
+            printResult(result);
+        }
+    }
+}
