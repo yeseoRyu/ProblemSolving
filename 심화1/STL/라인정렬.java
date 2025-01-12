1413a53d6ea333477386918fac9d7030450541a7
dolchvita <yesoe0502@naver.com>
2025-01-12 16:13:49 +0900

    0112

---
+++ 심화1/STL/라인정렬.java
@@ -0,0 +1,32 @@
+package STL;
+
+import java.util.*;
+
+public class 라인정렬 {
+    public static void main(String[] arg){
+        ArrayList<ArrayList<Integer>> arr = new ArrayList();
+        Scanner sc = new Scanner(System.in);
+
+        for(int i = 0; i < 5; i++){
+            ArrayList<Integer> list = new ArrayList<>();
+            String input = sc.next();
+            
+            for(int x = 0; x < input.length(); x++){
+                list.add(Integer.parseInt(String.valueOf(input.charAt(x))));
+            }
+            arr.add(list);
+        }
+        
+        int line1 = sc.nextInt(); 
+        int line2 = sc.nextInt();
+
+        int d  =1; 
+
+        Collections.sort(arr.get(line1));
+        Collections.sort(arr.get(line2));
+
+        for(int i = 0; i < 5; i++){
+            System.out.print(arr.get(i).get(0) + " ");
+        }
+    }   
+}
---
+++ 심화1/STL/숫자폭탄.java
@@ -0,0 +1,38 @@
+package STL;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class 숫자폭탄{
+    static void pang(ArrayList<Integer> result, int last){
+        for(int i = 0; i < 3; i++){
+            result.remove(last - i);
+        }
+    }
+
+    public static void main(String[] arg){
+        int n = 17;
+        int[] vect = {5, 4, 5, 1, 1, 1, 1, 1, 2, 2, 2, 3, 3, 3, 3, 8, 1};
+        ArrayList<Integer> tong = new ArrayList<>();
+
+        for(int i = 0; i < n; i++){
+            tong.add(vect[i]);
+
+            int tn = tong.size() - 1;
+            if(tn > 2){
+                int a = tong.get(tn);
+                int b = tong.get(tn - 1);
+                int c = tong.get(tn - 2);
+
+                if(a == b && b == c){
+                    pang(tong, tn);
+                }
+            }
+        }
+
+        Collections.sort(tong);
+        for(int i = 0; i < tong.size(); i++){
+            System.out.print(tong.get(i) + " ");
+        }
+    }
+}
