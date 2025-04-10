2dfe766ea4d24b82f7cb6693eaabbc4ee88007de
dolchvita <yesoe0502@naver.com>
2025-04-10 13:15:58 +0900

    0410

--- 훈련2/Tree_DFS/Lev2경로출력.java
+++ 훈련2/Tree_DFS/Lev2경로출력.java
@@ -4,19 +4,19 @@ public class Lev2경로출력 {
     /* 주어진 트리에서 2층에 도착시 경로 출력 */
     static int n;
     static int[][] map;
-    static int[] path = new int[10];
+    static int[] path = new int[3];
 
     // 층(lev)과 노드 번호(now)를 따로 관리하기!
     static void dfs(int lev, int now) {
         if (lev == 2) {
-            for (int i = 0; i <= 2; i++) {
+            for (int i = 0; i < 3; i++) {
                 System.out.print(path[i] + " ");
             }
             System.out.println();
         }
 
         for (int i = 0; i < n; i++) {
-            if (map[lev][i] == 0)
+            if (map[now][i] == 0)
                 continue;
 
             path[lev + 1] = i;
@@ -26,6 +26,8 @@ public class Lev2경로출력 {
     }
 
     public static void main(String[] args) {
+        /* 첫번째 경로 지정하기 (lev + 1 부터 담기므로) */
+        path[0] = 0;
         dfs(0, 0);
     }
 }
